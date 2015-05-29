fc2b4939329067705c7dc479ca73d11e380df2eb
vincentfree <vinsterdamus@gmail.com>
2015-05-29 12:13:19 +0200

    travis toevoeging
    Aanpassing in Node en LevelComparable toegevoegd

---
+++ src/main/java/com/vijfhart/casus/tree/LevelComparable.java
@@ -0,0 +1,8 @@
+package com.vijfhart.casus.tree;
+
+/**
+ * Created by Vincent Free on 29-5-2015.
+ */
+public interface LevelComparable extends Comparable<Node> {
+    public void compareLevelTo();
+}
--- src/main/java/com/vijfhart/casus/tree/Node.java
+++ src/main/java/com/vijfhart/casus/tree/Node.java
@@ -1,10 +1,11 @@
 package com.vijfhart.casus.tree;
 
 /**
- * Created by a591220 on 29-5-2015.
+ * Created by Vincent Free on 29-5-2015.
  */
-public interface Node <E extends Node<E>> {
+public interface Node <E extends Node<E>> extends Comparable <E>, LevelComparable {
 
     public E getParent();
-    public E setParent();
+    public E setParent(E node);
+    boolean isLeaf();
 }
