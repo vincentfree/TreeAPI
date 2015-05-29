f03f6c13732040abb831868d5d7b870a4f88a3e8
vincentfree <vinsterdamus@gmail.com>
2015-05-29 15:08:13 +0200

    TreeIterable aangepast en Tree aangemaakt

--- src/main/java/com/vijfhart/casus/tree/Tree.java
+++ src/main/java/com/vijfhart/casus/tree/Tree.java
@@ -1,9 +1,12 @@
 package com.vijfhart.casus.tree;
 
+import java.util.Collection;
+
 /**
  * Created by a591220 on 29-5-2015.
  */
-public interface Tree extends TreeIterable {
-    
+public interface Tree<E> extends TreeIterable {
+    //Collection collection();
+    Tree add(E node);
 
 }
