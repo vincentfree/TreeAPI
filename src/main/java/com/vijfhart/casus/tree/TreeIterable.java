3151f60451b5d1cc2a222426a0670c2418ff760e
vincentfree <vinsterdamus@gmail.com>
2015-05-29 14:02:27 +0200

    TreeIterable aangemaakt

---
+++ src/main/java/com/vijfhart/casus/tree/TreeIterable.java
@@ -0,0 +1,15 @@
+package com.vijfhart.casus.tree;
+
+import java.util.Iterator;
+import java.util.ListIterator;
+
+/**
+ * Created by Vincent Free on 29-5-2015.
+ */
+public interface TreeIterable extends Iterable {
+    /**
+     *
+     * @return Iterator of TreeIterator
+     */
+    Iterator iterator();
+}
--- src/main/java/com/vijfhart/casus/tree/TreeIterator.java
+++ src/main/java/com/vijfhart/casus/tree/TreeIterator.java
@@ -6,7 +6,7 @@ import java.util.Iterator;
  * Created by a591220 on 29-5-2015.
  */
 public interface TreeIterator<E extends Node<E>> extends Iterator<E> {
-    public abstract int level();
-    public abstract void startWith(E node);
-    public abstract boolean isLeaf();
+    int level();
+    void startWith(E node);
+    boolean isLeaf();
 }
